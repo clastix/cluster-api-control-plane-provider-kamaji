ae0c62bacc9056c38a7b947c8364a7ef2602e89d
Dario Tranchitella <dario@tranchitella.eu>
2023-06-26 10:02:45 +0200

    feat: kubevirt support

--- controllers/kamajicontrolplane_controller_cluster_patch.go
+++ controllers/kamajicontrolplane_controller_cluster_patch.go
@@ -34,9 +34,52 @@ func (r *KamajiControlPlaneReconciler) patchCluster(ctx context.Context, cluster
 	switch cluster.Spec.InfrastructureRef.Kind {
 	case "OpenStackCluster":
 		return r.patchOpenStackCluster(ctx, cluster, endpoint, port)
+	case "KubevirtCluster":
+		return r.patchKubeVirtCluster(ctx, cluster, endpoint, port)
 	default:
 		return errors.New("unsupported infrastructure provider")
 	}
+}
+
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kubevirtclusters,verbs=patch
+
+func (r *KamajiControlPlaneReconciler) patchKubeVirtCluster(ctx context.Context, cluster capiv1beta1.Cluster, endpoint string, port int64) error {
+	kvc := unstructured.Unstructured{}
+
+	kvc.SetGroupVersionKind(cluster.Spec.InfrastructureRef.GroupVersionKind())
+	kvc.SetName(cluster.Spec.InfrastructureRef.Name)
+	kvc.SetNamespace(cluster.Spec.InfrastructureRef.Namespace)
+
+	specPatch, err := json.Marshal(map[string]interface{}{
+		"spec": map[string]interface{}{
+			"controlPlaneEndpoint": map[string]interface{}{
+				"host": endpoint,
+				"port": port,
+			},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal KubeVirtCluster spec patch")
+	}
+
+	if err = r.client.Patch(ctx, &kvc, client.RawPatch(types.MergePatchType, specPatch)); err != nil {
+		return errors.Wrap(err, "cannot perform PATCH update for the KubeVirtCluster resource")
+	}
+
+	statusPatch, err := json.Marshal(map[string]interface{}{
+		"status": map[string]interface{}{
+			"ready": true,
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal KubeVirtCluster status patch")
+	}
+
+	if err = r.client.Status().Patch(ctx, &kvc, client.RawPatch(types.MergePatchType, statusPatch)); err != nil {
+		return errors.Wrap(err, "cannot perform PATCH update for the KubeVirtCluster status")
+	}
+
+	return nil
 }
 
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=openstackclusters,verbs=patch
