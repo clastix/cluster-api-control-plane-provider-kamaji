d2d416d5bfb3f4c448fe32480c0955514b38f2ad
Dario Tranchitella <dario@tranchitella.eu>
2023-08-04 15:31:23 +0200

    feat: aws support
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>

--- controllers/kamajicontrolplane_controller_cluster_patch.go
+++ controllers/kamajicontrolplane_controller_cluster_patch.go
@@ -34,6 +34,8 @@ func (r *KamajiControlPlaneReconciler) patchCluster(ctx context.Context, cluster
 	}
 
 	switch cluster.Spec.InfrastructureRef.Kind {
+	case "AWSCluster":
+		return r.patchGenericCluster(ctx, cluster, endpoint, port, false)
 	case "KubevirtCluster":
 		return r.patchGenericCluster(ctx, cluster, endpoint, port, true)
 	case "Metal3Cluster":
@@ -66,7 +68,7 @@ func (r *KamajiControlPlaneReconciler) checkOrPatchVSphereCluster(ctx context.Co
 	return nil
 }
 
-//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kubevirtclusters;nutanixclusters;packetclusters,verbs=patch
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kubevirtclusters;nutanixclusters;packetclusters;awsclusters,verbs=patch
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=kubevirtclusters/status;nutanixclusters/status;packetclusters/status,verbs=patch
 
 func (r *KamajiControlPlaneReconciler) patchGenericCluster(ctx context.Context, cluster capiv1beta1.Cluster, endpoint string, port int64, patchStatus bool) error {
