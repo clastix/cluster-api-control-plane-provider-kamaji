afd81361d822ba843ddf988686a47a0c4016b8d1
Dario Tranchitella <dario@tranchitella.eu>
2025-11-13 19:06:32 +0100

    feat: capi-operator v0.24.0 cli flags (#279)
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/component-base/featuregate"
 	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/util/flags"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	controlplanev1alpha1 "github.com/clastix/cluster-api-control-plane-provider-kamaji/api/v1alpha1"
@@ -50,9 +50,9 @@ func init() {
 func main() {
 	var dynamicInfraClusters []string
 
-	metricsAddr, enableLeaderElection, probeAddr, maxConcurrentReconciles := "", false, "", 1
+	metricsAddr, enableLeaderElection, probeAddr, maxConcurrentReconciles, managerOpts := "", false, "", 1, flags.ManagerOptions{}
 
-	flagSet := pflag.NewFlagSet("kamaji-control-plane-provider", pflag.ExitOnError)
+	flagSet := pflag.CommandLine
 
 	flagSet.StringSliceVar(&dynamicInfraClusters, "dynamic-infrastructure-clusters", nil, "When the DynamicInfrastructureClusterPatch feature flag is enabled, "+
 		"allows specifying which Infrastructure Clusters can be dynamically patched. "+
@@ -69,6 +69,7 @@ func main() {
 	opts := zap.Options{Development: true}
 	opts.BindFlags(&goFlagSet)
 
+	flags.AddManagerOptions(flagSet, &managerOpts)
 	flagSet.AddGoFlagSet(&goFlagSet)
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
@@ -108,6 +109,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	_, metricsOpts, err := flags.GetManagerOptions(managerOpts)
+	if err != nil {
+		setupLog.Error(err, "Unable to start manager: invalid flags")
+		os.Exit(1)
+	}
+
 	if !featureGate.Enabled(features.DynamicInfrastructureClusterPatch) && len(dynamicInfraClusters) > 0 {
 		setupLog.Error(errors.New("cannot set dynamic infrastructure clusters when the feature flag is disabled"), "DynamicInfrastructureClusterPatch feature flag is disabled")
 		os.Exit(1)
@@ -116,10 +123,8 @@ func main() {
 	ctx := ctrl.SetupSignalHandler()
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme: scheme,
-		Metrics: server.Options{
-			BindAddress: metricsAddr,
-		},
+		Scheme:  scheme,
+		Metrics: *metricsOpts,
 		WebhookServer: webhook.NewServer(webhook.Options{
 			Port: 9443, //nolint:mnd
 		}),
